128de3e31ae55db2fa5334b644fbc5d093ac205f
Bonsai <borjalamda@gmail.com>
2022-06-05 02:23:16 +0200

    feat(init): Fondo de inicio

--- src/constants/Login.constants.ts
+++ src/constants/Login.constants.ts
@@ -16,3 +16,109 @@ export const ERRORS = {
   NOMBRE_LARGO: 'El nombre es demasiado largo',
   ID_INVALIDO: 'El id de la sala no es valido',
 };
+
+export const PARTICLE_CONFIG: any = {
+  background: {
+    color: {
+      value: '#344E41',
+    },
+    position: '50% 50%',
+    repeat: 'no-repeat',
+    size: 'cover',
+  },
+  fullScreen: {
+    zIndex: -1,
+  },
+  interactivity: {
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40,
+        divs: {
+          distance: 200,
+          duration: 0.4,
+          mix: false,
+          selectors: [],
+        },
+      },
+      grab: {
+        distance: 400,
+      },
+      repulse: {
+        divs: {
+          distance: 200,
+          duration: 0.4,
+          factor: 100,
+          speed: 1,
+          maxSpeed: 50,
+          easing: 'ease-out-quad',
+          selectors: [],
+        },
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: '#A3B18A',
+    },
+    move: {
+      attract: {
+        rotate: {
+          x: 600,
+          y: 1200,
+        },
+      },
+      enable: true,
+      outModes: {
+        bottom: 'out',
+        left: 'out',
+        right: 'out',
+        top: 'out',
+      },
+      speed: 1,
+    },
+    number: {
+      density: {
+        enable: true,
+      },
+      value: 10,
+    },
+    opacity: {
+      random: {
+        enable: true,
+        minimumValue: 0.3,
+      },
+      value: {
+        min: 0.3,
+        max: 0.5,
+      },
+      animation: {
+        speed: 1,
+        minimumValue: 0.1,
+      },
+    },
+    shape: {
+      options: {
+        polygon: {
+          sides: 6,
+        },
+        star: {
+          sides: 5,
+        },
+      },
+      type: 'polygon',
+    },
+    size: {
+      random: {
+        enable: true,
+        minimumValue: 50
+      },
+      value: {
+        min: 50,
+        max: 100,
+      }
+    },
+  },
+};
--- src/pages/Login/Login.page.tsx
+++ src/pages/Login/Login.page.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
+import { loadFull } from "tsparticles";
 
 // Components
 import { LoginForm } from '../../components';
+import Particles from 'react-tsparticles';
 
 // Styles
 import "../../styles/Login.styles.css";
 
+// Constants
+import { PARTICLE_CONFIG } from '../../constants/Login.constants';
+
+
 const LoginPage: React.FC = () => {
+
+    const particlesInit = async (main: any) => {
+        console.log(main);
+        await loadFull(main);
+    };
+
+    const particlesLoaded = async (container: any) => {
+        console.log(container);
+    };
+
     return (
         <div id="login-page">
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={PARTICLE_CONFIG}
+            />
             <LoginForm />
         </div>
     );
