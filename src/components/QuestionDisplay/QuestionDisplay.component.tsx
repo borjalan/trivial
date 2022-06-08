276eae3f61376eef980b2e7c41fb7c9f63caf358
Magnolio <Caparazonverde@gmail.com>
2022-06-08 20:42:51 +0200

    Prueba jugadores

--- src/components/QuestionDisplay/QuestionDisplay.component.tsx
+++ src/components/QuestionDisplay/QuestionDisplay.component.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const QuestionDisplay: React.FC = () => {
     return (
-        <div id="question-display" style={{ backgroundColor: "#DAD7CD", height: "100%", width: "100%" }}></div>
+        <div id="question-display" style={{ backgroundColor: "#FFFFFF", height: "100%", width: "100%" }}></div>
     );
 };
 
